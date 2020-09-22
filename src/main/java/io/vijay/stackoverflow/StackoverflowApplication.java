70927d1f572de76384521d3840307024cff5ae82
vijay <vijay.thummar108554@marwadiuniversity.ac.in>
2020-09-22 18:27:52 +0530

    Remove unnecessary code

--- src/main/java/io/vijay/stackoverflow/StackoverflowApplication.java
+++ src/main/java/io/vijay/stackoverflow/StackoverflowApplication.java
@@ -9,9 +9,7 @@ public class StackoverflowApplication {
 
     public static void main(String[] args) {
 
-        User user1 = new User(13,"sfsdfs","sdfsdf","sdfsdfsdfsfsdfs",null);
 
-        System.out.println(user1);
 
         SpringApplication.run(StackoverflowApplication.class, args);
     }
